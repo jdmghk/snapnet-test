13a56a70b7dacb67bb3a4f284505a582627d711c
JDK <jacinthdavid70@gmail.com>
2024-10-23 12:19:52 +0100

    change in app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
-// const connectDB = require('./config/database');
 const dotenv = require("dotenv").config()
 const cors = require('cors');
 
 
 const port = process.env.PORT || 4000
 
-// connectDB();
+
 
 const app = express()
 
@@ -24,8 +23,8 @@ app.use((req, res, next)=> {
     next()
 });
 
-// app.use('/api/event', require('./routes/eventRoutes'));
-// app.use('/api/user', require('./routes/userRoutes'));
+app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/products', require('./routes/products'));
 // app.use('/api/ticket', require('./routes/ticketRoutes'));
 // app.use('/api/paystack', require('./routes/paystackRoutes'));
 // app.use('/', require('./routes/slugRoutes'));
